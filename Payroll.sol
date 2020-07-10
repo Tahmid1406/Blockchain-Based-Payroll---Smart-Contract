82c162f503a92eea6948e0edcc8a2dfe4db4651d
tahmid1406 <tahmidpranto1406@gmail.com>
2020-07-10 23:46:17 +0600

    payment added

--- Payroll.sol
+++ Payroll.sol
@@ -14,24 +14,33 @@ contract Department{
         uint remaining_balance;
     }
     
-   
+   event paySal(address _emp, uint value);
+   event payPen(address _emp, uint value);
     
     constructor() public{
         owner = msg.sender;
     }
  
+    modifier onlyOwner(){
+        require (msg.sender == owner, 'must be a owner');
+        _;
+    }
     
-    
-    function addDepartment(address ad, uint _dept_id, string memory _dept_name, uint _total_balance, uint _remaining_balance) public{
-        require(msg.sender == owner);
-        
+    function addDepartment(address ad, uint _dept_id, string memory _dept_name, uint _total_balance, uint _remaining_balance)
+    public onlyOwner{
         department[ad] = Dept_Struct(_dept_id, _dept_name, _total_balance, _remaining_balance);
         
     }
     
-    function paySallary(address _dept, address _emp, uint amount) payable external{
-        require(msg.sender == owner);
+    
+    function paySallary(address _dept, address _emp, uint amount) payable external onlyOwner{
         department[_dept].total_balance -= amount;
+        emit paySal(_emp, amount);
+    }
+    
+    function payPension(address _dept, address _emp, uint amount) payable external onlyOwner{
+        department[_dept].total_balance -= amount;
+        emit payPen(_emp, amount);
     }
     
 }
@@ -70,12 +79,17 @@ contract ActiveEmployee{
         owner = msg.sender;
     }
     
+    modifier onlyOwner(){
+        require (msg.sender == owner, 'must be a owner');
+        _;
+    }
+    
     
     function addActiveEmp(address _ad, uint _employee_id, uint _dept_id,
             string memory _name, string memory _birth_date, 
             uint _basic_salary, string memory _joining_date, 
             string memory _estimated_retire_date
-            ) public{
+            ) public onlyOwner{
                     
             activeEmp[_ad] = ActiveEmp(_ad, _employee_id, _dept_id, _name, _birth_date, 
                                                _basic_salary,_joining_date, _estimated_retire_date); 
@@ -83,19 +97,14 @@ contract ActiveEmployee{
     
     
      function employeeAgreement(address ad, uint _work_hour, uint _salllary, uint _paid_leave, 
-        uint _work_day, string memory _designation) public{
+        uint _work_day, string memory _designation) public onlyOwner{
          
-        require(msg.sender == owner);
         uint join_date = block.timestamp;
         empCont[ad] = EmployeeAgreement(_work_hour, _salllary, _paid_leave, _work_day, _designation,join_date); 
     }
     
-    function getEmployeeId(address ad) public returns(uint){
+    function getEmployeeId(address ad) view public onlyOwner returns(uint){
         return activeEmp[ad].employee_id;
-    }
-    
-    function paySallary(address dept_ad, address emp_ad) public{
-        
     }
     
    
@@ -129,38 +138,53 @@ contract RetiredEmploye{
         owner = msg.sender;
     }
     
-    function addEmp(address _ad, uint _employee_id, uint _dept_id, uint _total_balance, uint _remaining_balance,
-            bool _isforced_retired, bool _prior_conviction, bool _loan_organization, bool _loan_sister_organistion
-            ) public{
-            emp[ _ad] = EmpRetired(_ad, _employee_id, _dept_id, _total_balance, _remaining_balance, _isforced_retired,
-            _prior_conviction, _loan_organization, _loan_sister_organistion);
+    modifier onlyOwner(){
+        require (msg.sender == owner, 'must be a owner');
+        _;
+    }
+    
+    function addEmp(address _ad, uint _employee_id, 
+                    uint _dept_id, uint _total_balance, 
+                    uint _remaining_balance,
+                    bool _isforced_retired, 
+                    bool _prior_conviction, 
+                    bool _loan_organization, 
+                    bool _loan_sister_organistion) 
+                    
+                    public onlyOwner{
+                
+            emp[ _ad] = EmpRetired(_ad, _employee_id, 
+                                   _dept_id, _total_balance, 
+                                   _remaining_balance, _isforced_retired,
+                                   _prior_conviction, _loan_organization,
+                                   _loan_sister_organistion);
             
             PensionStatus[_ad] = false;
     }
     
-    function getPrioConvictionStatus(address ad) public returns(bool){
+    function getPrioConvictionStatus(address ad) view public returns(bool){
         require(msg.sender == owner);
         return emp[ad].prior_conviction;
     }
     
-    function getPriorInternalLoanStatus(address ad) public returns(bool){
+    function getPriorInternalLoanStatus(address ad) view public returns(bool){
         require(msg.sender == owner);
         return emp[ad].loan_organization;
     }
     
-    function getPriorExternalLoanStatus(address ad) public returns(bool){
+    function getPriorExternalLoanStatus(address ad) view public returns(bool){
         require(msg.sender == owner);
         return emp[ad].loan_sister_organistion;
     }
     
-    function getAllStatusClear(address ad) public returns(bool){
+    function getAllStatusClear(address ad) view public returns(bool){
         require(msg.sender == owner);
         if(emp[ad].isforced_retired == false && emp[ad].prior_conviction == false){
             return emp[ad].loan_sister_organistion;
         }
     }
     
-    function getRetirementType(address ad) public returns(string){
+    function getRetirementType(address ad) view public returns(string){
         require(msg.sender == owner);
         if(emp[ad].isforced_retired == true){
             return "Early Retirement";
@@ -169,7 +193,7 @@ contract RetiredEmploye{
         }
     }
     
-    function calculatePensionAmount(address ad, uint loan_internal, uint loan_external) public returns(uint){
+    function calculatePensionAmount(address ad, uint loan_internal, uint loan_external)view public returns(uint){
         uint total = emp[ad].remaining_balance;
         uint ret = total - (loan_internal + loan_external);
         return ret;
